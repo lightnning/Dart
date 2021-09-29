e2839c19065390355886d4e82286d7516c041174
lightnning <spde6g59@arrow.ocn.ne.jp>
2021-09-30 08:46:47 +0900

    UpcastとDowncastの違い

--- lib/class-test.dart
+++ lib/class-test.dart
@@ -1,12 +1,14 @@
-import 'dart:html';
+// 参照url:https://zenn.dev/iwaku/articles/2020-12-16-iwaku
+// クラスを利用したクラス作成の基本
+
 
 class SuperClass {
   void test() => print('SuperClass');
-
 }
 
 class SubClass extends SuperClass {
-  @override test() => print('SubClass'); 
+  @override
+  test() => print('SubClass');
 }
 
 void main() {
@@ -14,5 +16,20 @@ void main() {
   SuperClass obj1 = SuperClass();
   obj1.test();
   SubClass obj2 = SubClass();
+  obj2.test();
+
+  // Upcast
+  // サブクラスのオブジェクトをスーパークラスと同じように扱うこと
+  SuperClass obj3 = SubClass();
+  obj3.test();
   
+  // Downcast
+  // スーパークラスのオブジェクトをサブクラスと同じように扱うこと
+  //  =スーパークラスのオブジェクトをサブクラスのオブジェクトに対応できるように変換
+  // 自動で行われない
+
+  // SubClass obj4 = obj1; // => エラーになる 
+  // obj4.test(); 
+
+
 }
