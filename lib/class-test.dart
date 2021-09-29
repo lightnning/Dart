cdf446d4d648e8ec1a42d6b5c915187efafeab85
lightnning <spde6g59@arrow.ocn.ne.jp>
2021-09-30 08:50:13 +0900

    スーパークラスの関数を呼び出す

--- lib/class-test.dart
+++ lib/class-test.dart
@@ -8,28 +8,16 @@ class SuperClass {
 
 class SubClass extends SuperClass {
   @override
-  test() => print('SubClass');
+  // test() => print('SubClass');
+  void test() {
+    super.test();
+    print('SubClass');
+  }
 }
 
 void main() {
-  // 通常利用
-  SuperClass obj1 = SuperClass();
-  obj1.test();
-  SubClass obj2 = SubClass();
-  obj2.test();
-
-  // Upcast
-  // サブクラスのオブジェクトをスーパークラスと同じように扱うこと
-  SuperClass obj3 = SubClass();
-  obj3.test();
-  
-  // Downcast
-  // スーパークラスのオブジェクトをサブクラスと同じように扱うこと
-  //  =スーパークラスのオブジェクトをサブクラスのオブジェクトに対応できるように変換
-  // 自動で行われない
-
-  // SubClass obj4 = obj1; // => エラーになる 
-  // obj4.test(); 
+  SubClass obj = SubClass();
+  obj.test();
 
 
 }
