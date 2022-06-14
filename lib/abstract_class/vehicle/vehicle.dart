4815ad5a2cdc96448c5a2f30b1207b20f5bbf7dc
Tomoya_Kobayashi <spde6g59@arrow.ocn.ne.jp>
2022-06-15 06:57:29 +0900

    vehicleクラス作成

---
+++ lib/abstract_class/vehicle/vehicle.dart
@@ -0,0 +1,15 @@
+abstract class Vehicle{
+  void showStatus();
+}
+
+class Car extends Vehicle{
+  @override 
+  void showStatus(){
+    print('.....');
+  }
+}
+
+void main(){
+  var a = Car();
+  a.showStatus();
+}
